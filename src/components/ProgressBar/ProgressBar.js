12b4800d1825f1faa4d7276b9b94dafe66626e65
Hugh Nguyen <hughnguyen@meta.com>
2024-12-28 21:02:01 -0600

    Progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,50 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZE_TO_HEIGHT = {
+  small: 8,
+  medium: 12,
+  large: 24,
+}
+
+const SIZE_TO_PADDING = {
+  small: 0,
+  medium: 0,
+  large: 4,
+}
+
+const SIZE_TO_BORDER_RADIUS = {
+  small: 4,
+  medium: 4,
+  large: 8,
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Background size={size}>
+    <FillWrapper size={size}>
+      <Fill style={{ width: `${value}%` }} />
+    </FillWrapper>
+  </Background>;
 };
+
+const FillWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius:  ${({size}) => SIZE_TO_BORDER_RADIUS[size] - SIZE_TO_PADDING[size]}px;;
+  overflow: hidden;
+`
+
+const Fill = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+`
+
+const Background = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius:  ${({size}) => SIZE_TO_BORDER_RADIUS[size]}px;;
+  padding: ${({size}) => SIZE_TO_PADDING[size]}px;
+  height: ${({size}) => SIZE_TO_HEIGHT[size]}px;
+`
 
 export default ProgressBar;
